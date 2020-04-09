e612a124402808730e074ccc6d9b5b1c03c703f7
Luis Vallejo <vallejoaguilar@gmail.com>
2020-04-08 21:36:53 -0500

    rutas template

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../template/header';
 import Home from '../pages/home';
 import Character from '../pages/character';
 import Error404 from '../pages/error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
     '/': Home,
@@ -11,9 +13,13 @@ const routes = {
 
 const router = async () => {
     const header = null || document.getElementById('header');
-    const content = null || document.getElementById('contect');
+    const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
 };
 
 
